5aca6d3e18eaeb9ee8f9aed0e8093c7b9352ea3a
ELCOT <ELCOT@Lenovo.mshome.net>
2024-07-25 16:19:22 +0530

    commit

---
+++ Constructor/src/com/constructor/MainDemo.java
@@ -0,0 +1,12 @@
+package com.constructor;
+
+public class MainDemo {
+
+	public static void main(String[] args) {
+		Demo d1=new Demo();
+		Demo d2=new Demo(1);
+		Demo d3=new Demo(1,"Rakesh");
+		Demo d4=new Demo("Sharma");
+	}
+
+}
